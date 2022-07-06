4872cfe5cc322d656b891251cf03b846000249a8
meohin <meohin.meo@gmail.com>
2022-07-06 17:53:03 +0900

    Add mini test of remove key of hashmap

--- src/main/java/IndexingString.java
+++ src/main/java/IndexingString.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 
 public class IndexingString {
     public void getGenderNumber(String pin) {
@@ -11,19 +12,28 @@ public class IndexingString {
         System.out.println(a.replaceAll(":", "#"));
     }
 
-    public void makeDescending(){
+    public void makeDescending() {
         ArrayList<Integer> myList = new ArrayList<>(Arrays.asList(1, 3, 5, 4, 2));
 
         myList.sort(Comparator.reverseOrder());
         System.out.println(myList);
     }
 
-
-    public void combineString(){
+    public void combineString() {
         ArrayList<String> myList = new ArrayList<>(Arrays.asList("Life", "is", "too", "short"));
 
         String result = String.join(",", myList);
         System.out.println(result);
+    }
+
+    public void searchKey() {
+        HashMap<String, Integer> grade = new HashMap<>();
+        grade.put("A", 90);
+        grade.put("B", 80);
+        grade.put("C", 70);
+
+        grade.remove("B");
+        System.out.println(grade);
     }
 
     public static void main(String[] args) {
@@ -38,5 +48,7 @@ public class IndexingString {
         indexingString.makeDescending();
 
         indexingString.combineString();
+
+        indexingString.searchKey();
     }
 }
