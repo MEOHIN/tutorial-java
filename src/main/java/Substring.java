f7ed1af4a581d797c0a6e733d0b86da2e7cd492e
meohin <meohin.meo@gmail.com>
2022-07-06 16:54:55 +0900

    Add mini test of separate id number by hyphen

---
+++ src/main/java/Substring.java
@@ -0,0 +1,19 @@
+public class Substring {
+
+    public void separateIdNum(String id) {
+
+        String front = id.substring(0, 6);
+        String back = id.substring(7, 14);
+
+        System.out.println("19" + front);
+        System.out.println(back);
+    }
+
+    public static void main(String[] args) {
+
+        String id = "881120-1068234";
+
+        Substring substring = new Substring();
+        substring.separateIdNum(id);
+    }
+}
