169ada534e360462bdbc172d6eebd4715c463e25
meohin <meohin.meo@gmail.com>
2022-07-06 17:48:01 +0900

    Add mini test of descending

--- src/main/java/IndexingString.java
+++ src/main/java/IndexingString.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class IndexingString {
     public void getGenderNumber(String pin) {
         System.out.println(pin.charAt(7));
@@ -5,6 +9,13 @@ public class IndexingString {
 
     public void changeString(String a) {
         System.out.println(a.replaceAll(":", "#"));
+    }
+
+    public void makeDescending(){
+        ArrayList<Integer> myList = new ArrayList<>(Arrays.asList(1, 3, 5, 4, 2));
+
+        myList.sort(Comparator.reverseOrder());
+        System.out.println(myList);
     }
 
     public static void main(String[] args) {
@@ -15,5 +26,7 @@ public class IndexingString {
 
         String a = "a:b:c:d";
         indexingString.changeString(a);
+
+        indexingString.makeDescending();
     }
 }
