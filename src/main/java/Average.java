a1560e44a6147aa47c199156335c44f65591f05c
meohin <meohin.meo@gmail.com>
2022-07-06 16:11:56 +0900

    Add mini test of average score

---
+++ src/main/java/Average.java
@@ -0,0 +1,18 @@
+public class Average {
+
+    public void AverageScore (int kor, int eng, int math) {
+        int sum = kor + eng + math;
+        int total = sum/3;
+
+        System.out.println("홍길동 씨의 평균 점수: " + total);
+    }
+
+    public static void main(String[] args) {
+        int kor = 80;
+        int eng = 75;
+        int math = 55;
+
+        Average average = new Average();
+        average.AverageScore(kor, eng, math);
+    }
+}
