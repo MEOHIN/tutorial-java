efeb01e3af7c105383a308747e7d6824461f51c0
meohin <meohin.meo@gmail.com>
2022-07-07 18:26:51 +0900

    Add mini test of average score

---
+++ src/main/java/controlFlow/averageScore.java
@@ -0,0 +1,18 @@
+package controlFlow;
+
+public class averageScore {
+    public static void main(String[] args) {
+        int[] marks = {70, 60, 55, 75, 95, 90, 80, 80, 85, 100};
+        int sum = 0;
+
+        for (int score : marks
+             ) {
+            sum += score;
+
+            if (score == 100) {
+                int average = sum/marks.length;
+                System.out.println(average);
+            }
+        }
+    }
+}
