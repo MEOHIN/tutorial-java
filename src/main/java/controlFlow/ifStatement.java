cc7fcb50bed973fc524d1dd6fc094086102c8a89
meohin <meohin.meo@gmail.com>
2022-07-07 17:29:53 +0900

    Add mini test of if conditional

---
+++ src/main/java/controlFlow/ifStatement.java
@@ -0,0 +1,19 @@
+package controlFlow;
+
+public class ifStatement {
+    public static void main(String[] args) {
+        String a = "Write once, run anywhere";
+
+        if (a.contains("wife")) {
+            System.out.println("wife");
+        } else if (a.contains("once") && !a.contains("run")) {
+            System.out.println("once");
+        } else if (!a.contains("everywhere")) {
+            System.out.println("everywhere");
+        } else if (a.contains("anywhere")) {
+            System.out.println("anywhere");
+        } else {
+            System.out.println("none");
+        }
+    }
+}
