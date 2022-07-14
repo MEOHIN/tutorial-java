5dcf936b509f8681cf34a58ef4f74651a667da23
meohin <meohin.meo@gmail.com>
2022-07-14 14:37:11 +0900

    Add code for Interface

--- src/main/java/objectOrientedProgrming/Fruit.java
+++ src/main/java/objectOrientedProgrming/Fruit.java
@@ -1,5 +1,9 @@
 package objectOrientedProgrming;
 
+interface Tropical {
+    String getFactor();
+}
+
 class Fruit {
     String name;
 
@@ -8,30 +12,20 @@ class Fruit {
     }
 }
 
-class Apple extends Fruit {
-    void ripe() {
-        System.out.println(this.name+" yum");
+class Avocado extends Fruit implements Tropical {
+    public String getFactor() {
+        return "water";
     }
 }
 
-class HomegrownApple extends Apple {
-    HomegrownApple(String name) {
-        this.setName(name);
+class Banana extends Fruit implements Tropical{
+    public String getFactor() {
+        return "sunlight";
     }
+}
 
-    HomegrownApple(int type) {
-        if (type == 1) {
-            this.setName("envy");
-        } else if (type == 2) {
-            this.setName("fuji");
-        }
-    }
-
-    void ripe() {
-        System.out.println(this.name+" yum in my yard");
-    }
-
-    void ripe(String when) {    // 기존 ripe() 메서드와 이름은 동일하지만, 입력항목이 다르다.
-        System.out.println(this.name+" yum in my yard" + when);
+class Farmer {
+    void grow(Tropical tropical) {
+        System.out.println("grow " + tropical.getFactor());
     }
 }
--- src/main/java/objectOrientedProgrming/Sample.java
+++ src/main/java/objectOrientedProgrming/Sample.java
@@ -2,9 +2,8 @@ package objectOrientedProgrming;
 
 public class Sample {
     public static void main(String[] args) {
-        HomegrownApple red = new HomegrownApple("red");
-        HomegrownApple envy = new HomegrownApple("1");
-        System.out.println(red.name);    // red 출력
-        System.out.println(envy.name);    // envy 출력
+        Farmer farmer = new Farmer();
+        Avocado avocado= new Avocado();
+        Banana banana = new Banana();
     }
 }
