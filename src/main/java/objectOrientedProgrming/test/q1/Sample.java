19a2f2aa070c67bf3080448625425bae648f777f
meohin <meohin.meo@gmail.com>
2022-07-14 17:47:43 +0900

    Add exercise code for inheritance

---
+++ src/main/java/objectOrientedProgrming/test/q1/Sample.java
@@ -0,0 +1,32 @@
+package objectOrientedProgrming.test.q1;
+
+class Calculator {
+    int value;
+
+    Calculator() {
+        this.value = 0;
+    }
+
+    void add(int val) {
+        this.value += val;
+    }
+
+    int getValue() {
+        return this.value;
+    }
+}
+
+class UpgradeCalculator extends Calculator{
+    void minus(int num) {
+        this.value -= num;
+    }
+}
+
+public class Sample {
+    public static void main(String[] args) {
+        UpgradeCalculator cal = new UpgradeCalculator();
+        cal.add(10);
+        cal.minus(3);
+        System.out.println(cal.getValue());  // 10에서 3을 뺀 7을 출력
+    }
+}
