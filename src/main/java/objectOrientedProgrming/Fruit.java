2a9c54f0e49ecab8772bf9b6af1b231d9e7a3ddb
meohin <meohin.meo@gmail.com>
2022-07-14 14:32:22 +0900

    Add code for Inheritance

--- src/main/java/objectOrientedProgrming/Fruit.java
+++ src/main/java/objectOrientedProgrming/Fruit.java
@@ -3,7 +3,23 @@ package objectOrientedProgrming;
 class Fruit {
     String name;
 
-    public void setName(String name) {
+    void setName(String name) {
         this.name = name;
+    }
+}
+
+class Apple extends Fruit {
+    void ripe() {
+        System.out.println(this.name+" yum");
+    }
+}
+
+class HomegrownApple extends Apple {
+    void ripe() {
+        System.out.println(this.name+" yum in my yard");
+    }
+
+    void ripe(String when) {    // 기존 ripe() 메서드와 이름은 동일하지만, 입력항목이 다르다.
+        System.out.println(this.name+" yum in my yard" + when);
     }
 }
--- src/main/java/objectOrientedProgrming/Sample.java
+++ src/main/java/objectOrientedProgrming/Sample.java
@@ -2,13 +2,9 @@ package objectOrientedProgrming;
 
 public class Sample {
     public static void main(String[] args) {
-        Fruit apple = new Fruit();
-        apple.setName("pip");
-
-        Fruit banana = new Fruit();
-        banana.setName("pulp");
-
-        System.out.println(apple.name);    // pip 출력
-        System.out.println(banana.name);    // pulp 출력
+        HomegrownApple homeApple = new HomegrownApple();
+        homeApple.setName("red");    // red
+        homeApple.ripe();    // apple yum in my yard
+        homeApple.ripe("this morning");    // apple yum in my yard this morning
     }
 }
