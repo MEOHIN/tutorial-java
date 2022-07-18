b5e0594e5e3dc370e936a2f4c5cded07e5aaea76
meohin <meohin.meo@gmail.com>
2022-07-18 17:57:20 +0900

    Add exercise for file write and read with input and add mode

---
+++ src/main/java/inoutput/test4.java
@@ -0,0 +1,28 @@
+package inoutput;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class test4 {
+    public static void main(String[] args) throws IOException {
+        String path = "C:\\projects\\jumpto\\java\\datatype\\";
+        PrintWriter pw = new PrintWriter(new FileWriter(path + "sample.txt", true));
+
+        System.out.println("입력하세요:");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        pw.println(input);
+        pw.close();
+
+        BufferedReader br = new BufferedReader(new FileReader(path + "sample.txt"));
+        while (true) {
+            String line = br.readLine();
+            if (line == null) {
+                break;
+            }
+            System.out.println(line);
+        }
+        br.close();
+    }
+}
