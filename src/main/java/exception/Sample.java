0676c274c2c6846e5943e65d511e0baa08a375ff
meohin <meohin.meo@gmail.com>
2022-07-19 17:04:46 +0900

    Add exercise for Exception

--- src/main/java/exception/Sample.java
+++ src/main/java/exception/Sample.java
@@ -1,14 +1,18 @@
 package exception;
 
-class HungryException extends RuntimeException {
+class HungryException extends Exception {
 }
 
 public class Sample {
     public void eatFood(String food) {
-        if("full".equals(food)) {
-            throw new HungryException();
+        try {
+            if("full".equals(food)) {
+                throw new HungryException();
+            }
+            System.out.println("You are " + food);
+        } catch (HungryException e) {
+            System.out.println("HungryException 발생");
         }
-        System.out.println("You are " + food);
     }
 
     public static void main(String[] args) {
