cbc02290b6893fab8048dd4dc366fc1ee5e93c8c
meohin <meohin.meo@gmail.com>
2022-07-20 16:34:37 +0900

    Add exercise for lambda input type skip

--- src/main/java/lambda/Sample.java
+++ src/main/java/lambda/Sample.java
@@ -1,12 +1,13 @@
 package lambda;
 
+@FunctionalInterface
 interface Calculator {
     int sum(int a, int b);
 }
 
 public class Sample {
     public static void main(String[] args) {
-        Calculator mc = (int a, int b) -> a + b;
+        Calculator mc = (a, b) -> a + b;
         int result =  mc.sum(3, 4);
         System.out.println(result);    // 7 출력
     }
