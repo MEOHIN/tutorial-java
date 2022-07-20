28eb6c2c43464cec88973614ec0fac6cce9351f9
meohin <meohin.meo@gmail.com>
2022-07-20 16:39:18 +0900

    Add exercise for lambda into method reference

--- src/main/java/lambda/Sample.java
+++ src/main/java/lambda/Sample.java
@@ -7,7 +7,7 @@ interface Calculator {
 
 public class Sample {
     public static void main(String[] args) {
-        Calculator mc = (a, b) -> a + b;
+        Calculator mc = Integer::sum;
         int result =  mc.sum(3, 4);
         System.out.println(result);    // 7 출력
     }
