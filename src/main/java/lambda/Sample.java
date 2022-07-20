502095893e9db6dff1e2092e62a440ac4587c2cb
meohin <meohin.meo@gmail.com>
2022-07-20 16:25:45 +0900

    Add exercise for lambda

--- src/main/java/lambda/Sample.java
+++ src/main/java/lambda/Sample.java
@@ -4,15 +4,9 @@ interface Calculator {
     int sum(int a, int b);
 }
 
-class MyCalculator implements Calculator {
-    public int sum(int a, int b) {
-        return a+b;
-    }
-}
-
 public class Sample {
     public static void main(String[] args) {
-        MyCalculator mc = new MyCalculator();
+        Calculator mc = (int a, int b) -> a + b;
         int result =  mc.sum(3, 4);
         System.out.println(result);    // 7 출력
     }
