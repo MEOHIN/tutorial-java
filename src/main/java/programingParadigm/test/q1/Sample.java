0c0a8f9fd193367793fb916bae74a0282c3ba363
meohin <meohin.meo@gmail.com>
2022-07-21 18:39:18 +0900

    Add exercise for package

---
+++ src/main/java/programingParadigm/test/q1/Sample.java
@@ -0,0 +1,10 @@
+package programingParadigm.test.q1;
+
+import programingParadigm.test.q1.house.HousePark;
+
+public class Sample {
+    public static void main(String[] args) {
+        HousePark housePark = new HousePark();
+        System.out.println(housePark.getLastname());
+    }
+}
---
+++ src/main/java/programingParadigm/test/q1/house/HousePark.java
@@ -0,0 +1,9 @@
+package programingParadigm.test.q1.house;
+
+public class HousePark {
+    protected String lastname = "park";
+
+    public String getLastname() {
+        return this.lastname;
+    }
+}
