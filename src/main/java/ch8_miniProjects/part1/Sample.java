888adf833f191f29fa5b7184dd2aa74d8c2bda9a
meohin <meohin.meo@gmail.com>
2022-07-22 17:42:59 +0900

    Add description of the question

--- src/main/java/ch8_miniProjects/part1/Sample.java
+++ src/main/java/ch8_miniProjects/part1/Sample.java
@@ -1,5 +1,9 @@
 package ch8_miniProjects.part1;
 
+/***
+ * 1000 미만의 자연수에서 3의 배수와 5의 배수의 총합을 구하라.
+ */
+
 public class Sample {
     public static void main(String[] args) {
         int result = 0;
