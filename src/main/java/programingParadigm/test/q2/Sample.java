eee2491bd26a73983a2bdc60088385b3c02f6346
meohin <meohin.meo@gmail.com>
2022-07-22 16:19:11 +0900

    Add exercise for finally of try/catch

---
+++ src/main/java/programingParadigm/test/q2/Sample.java
@@ -0,0 +1,26 @@
+package programingParadigm.test.q2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Sample {
+    public static void main(String[] args) {
+        int result = 0;
+        try {
+            int[] a = {1, 2, 3};
+            int b = a[3];
+            ArrayList c = new ArrayList(Arrays.asList("3"));
+            int d = (int) c.get(0);
+            int e = 4 / 0;
+        } catch (ClassCastException e) {
+            result += 1;
+        } catch (ArithmeticException e) {
+            result += 2;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            result += 3;
+        } finally {
+            result += 4;
+        }
+        System.out.println(result); // result 의 값은 무엇일까?
+    }
+}
